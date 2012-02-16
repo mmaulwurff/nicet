312c78d8311ebd66322fbea3b0464b8c0903e206
mmaulwurff <mmaulwurff@gmail.com>
2012-02-16 18:54:39 +0700

    lazy mode, other features

--- nicet.c
+++ nicet.c
@@ -1,4 +1,4 @@
-/* nicet, a nice tetris
+/* nicet, nice tetris
  * Copyright (C) 2012  Alexander Kromm (mmaulwurff[at]gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -139,21 +139,22 @@ char map[24][16]={
 	"###          ###",
 	"###          ###",
 	"###          ###",
-	"###          ###",
+	"###          ###", //20
 	"################",
 	"################",
 	"################",
 };
 
 WINDOW * window, * info;
-short x, y;
-short active, next;
-short fallen_flag;
-short count=0;
+short x, y; //left upper corner of active block
+short active, next; //blocks
+short fallen_flag; //1 if have just fallen. needed to compensate falling sometimes
+short count=0; //for not to fall everytime player presses a key
 short score=0;
-short delay=10;
+short delay=10; //0.1 seconds, speed=11-delay
+char mode; //'l' for lazy mode, other for normal
 
-int color(int in) {
+int color(int in) { //colors of blocks. used in wattrset
 	if (in==0) return 1;
 	else if (in<3)  return 2;
 	else if (in<5)  return 3;
@@ -163,7 +164,7 @@ int color(int in) {
 	return 7;
 }
 
-void upd_part() { //updates only what is needed
+void upd_part() { //updates only current block position
 	short i, j;
 	//erase
 	wstandend(window);
@@ -171,7 +172,7 @@ void upd_part() { //updates only what is needed
 	for (j=y-1; j<=y+3; ++j)
 		if (map[j][i]==' ') mvwprintw(window, j, 2*(i-2)-1, "  ");
 	//print new
-	wattrset(window, COLOR_PAIR(color(active))); //choose color
+	wattrset(window, COLOR_PAIR(color(active)));
 	for (i=0; i<4; ++i)
 	for (j=0; j<4; ++j)
 		if (blocks[active][j][i]=='#') mvwprintw(window, y+j, 2*(x+i-2)-1, "##");
@@ -180,21 +181,26 @@ void upd_part() { //updates only what is needed
 	wrefresh(window);
 }
 
-void print_next() {
+void print_next() { //next block preview
 	wstandend(info);
 	werase(info);
 	box(info, 0, 0);
 	mvwprintw(info, 0, 0, "Score:%4d", score);
 	mvwprintw(info, 5, 1, "Speed:%2d", 11-delay);
-	wattrset(info, COLOR_PAIR(color(next)));
-	short i, j;
-	for (i=0; i<4; ++i)
-	for (j=0; j<4; ++j)
-		if (blocks[next][j][i]=='#') mvwprintw(info, j+1, 2*i+1, "##");
+	if (mode!='l') {
+		wattrset(info, COLOR_PAIR(color(next)));
+		short i, j;
+		for (i=0; i<4; ++i)
+		for (j=0; j<4; ++j)
+			if (blocks[next][j][i]=='#') mvwprintw(info, j+1, 2*i+1, "##");
+	} else {
+		mvwprintw(info, 2, 3, "Lazy");
+		mvwprintw(info, 3, 3, "Mode");
+	}
 	wrefresh(info);
 }
 
-void upd_all() {
+void upd_all() { //update all map
 	short i, j;
 	for (j=1; j<21; ++j)
 	for (i=3; i<13; ++i)
@@ -208,7 +214,7 @@ void upd_all() {
 	wrefresh(window);
 }
 
-int check(int side, int bottom) { //side -1 means left side, 1 means right
+int check(int side, int bottom) { //check fitness of block position and a map
 	short i, j;
 	for (i=0; i<4; ++i)
 	for (j=0; j<4; ++j)
@@ -216,7 +222,7 @@ int check(int side, int bottom) { //side -1 means left side, 1 means right
 	return 1;
 }
 
-int clockwise() {
+int clockwise() { //turn
 	switch (active) {
 		case  1: active=2;  break;
 		case  2: active=1;  break;
@@ -245,7 +251,7 @@ int clockwise() {
 	}
 }
 
-int aclockwise() {
+int aclockwise() { //turn anticlockwise
 	switch (active) {
 		case 1:  active=2;  break;
 		case 2:  active=1;  break;
@@ -274,23 +280,22 @@ int aclockwise() {
 	}
 }
 
-int check_clock() {
+int check_clock() { //check clockwise turn possibility
 	clockwise();
 	int ret=check(0, 0);
 	aclockwise();
 	return ret;
 }
 
-int check_aclock() {
+int check_aclock() { //check anticlockwise turn possibility
 	aclockwise();
 	int ret=check(0, 0);
 	clockwise();
 	return ret;
 }
 
-inline stop() {
+inline stop() { //make current active block a part of a map, remove lines if possible
 	short i, j, flag;
-	//make block a part of the map
 	for (i=0; i<4; ++i)
 	for (j=0; j<4; ++j)
 		if (blocks[active][j][i]=='#') map[y+j][x+i]=color(active);
@@ -323,7 +328,60 @@ void fall_comp() { //compensate falling
 	count=(count<4) ? count+1 : 0;
 }
 
-void key_act() {
+int rand_num() { //random number
+	FILE * file=fopen("/dev/urandom", "r");
+	int temp=fgetc(file);
+	fclose(file);
+	return temp;
+}
+
+int rand_active() { //random block
+	short temp=rand_num()%21;
+	if (temp>18) return 0; //for 'O' block to spawn more often
+	return temp;
+}
+
+int check_to_up(int j, int i) { //check if column is free
+	short k;
+	for (k=j; k>0; --k) if (map[k][i]!=' ') return 0;
+	return 1;
+}
+
+int next_easy() { //returns number of the most suitable block
+	short i, j;
+	short max_deep=0, hole_x;
+	for (i=3; i<13; ++i) {
+		for (j=0; map[j+1][i]==' '; ++j);
+		if (j>=max_deep) {
+			max_deep=j;
+			hole_x=i;
+		}
+	}
+	short length=0;
+	while (map[max_deep][hole_x-length]==' ') ++length;
+	//now we have everything to determine the best block
+	if (length==4) return 5+rand_num()%2; //I blocks
+	else if (length==3) return 7+rand_num()%8; //T, J, L blocks
+	else if (length==2) {
+		if (check_to_up(max_deep-1, hole_x-2) &&
+		    check_to_up(max_deep-1, hole_x+1)) return rand_num()%5; //O, S, Z
+		else if (check_to_up(max_deep-1, hole_x-2)) return 3+rand_num()%2; //Z
+		else if (check_to_up(max_deep-1, hole_x+1)) return rand_num()%3; //S, O
+		else return 0; //O block
+	}
+	else if (length==1) {
+		if (check_to_up(max_deep-1, hole_x-1) ||
+		    check_to_up(max_deep-1, hole_x+1)) return 15+rand_num()%4; //T
+		else if (check_to_up(max_deep-2, hole_x-1))
+			return 11+rand_num()%4; //L
+		else if (check_to_up(max_deep-2, hole_x+1))
+			return 7+rand_num()%4; //J
+		else return 5+rand_num()%2; //I
+	}
+	return rand_active();
+}
+
+void key_act() { //get key and do something with it
 	int c=getch();
 	switch (c) {
 		case 'Q': delwin(window); endwin(); exit(0); break;
@@ -339,26 +397,43 @@ void key_act() {
 			if (!check(0, 0)) clockwise();
 			fall_comp();
 		break;
-		default: break;
+		case 'd':
+			fall_comp();
+			if (mode!='l' && score>21) {
+				active=next_easy();
+				score-=20;
+			}
+		break;
+		case 'p':
+			fall_comp();
+			wclear(window);
+			box(window, 0, 0);
+			mvwprintw(window, 3, 7, "Pause");
+			wrefresh(window);
+			while ('p'!=getch());
+			upd_all();
+		break;
+		case KEY_DOWN: case ERR: break;
+		default: fall_comp(); break;
 	}
 }
 
-int rand_active() {
-	FILE * file=fopen("/dev/urandom", "r");
-	int temp=fgetc(file)%21;
-	fclose(file);
-	if (temp>18) return 0; //for 'O' block to spawn more often
-	return temp;
-}
-
-main() {
+main(int argc, char * argv[]) {
 	printf("\nnicet\nCopyright (C) 2012 Alexander Kromm (mmaulwurff[at]gmail.com)\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; see COPYING for details.\n\n");
-	initscr();
+	initscr(); //start ncurses screen
 	start_color();
-	noecho();
-	keypad(stdscr, TRUE);
-	curs_set(0);
-	halfdelay(delay);
+	noecho(); //for characters not to appear at the screen
+	keypad(stdscr, TRUE); //access to additional keys
+	curs_set(0); //not to show cursor
+	short i;
+	for (i=1; i<argc; ++i) if (argv[i][0]=='-') {
+		if (argv[i][1]=='l') mode='l';
+		else if (argv[i][1]=='s') {
+			short temp=atoi(argv[++i]);
+			if (temp<11 && temp>0) delay=11-temp;
+		}
+	}
+	halfdelay(delay); //game speed
 	init_pair(1, COLOR_YELLOW,  COLOR_YELLOW);
 	init_pair(2, COLOR_BLUE,    COLOR_BLUE);
 	init_pair(3, COLOR_MAGENTA, COLOR_MAGENTA);
@@ -372,13 +447,17 @@ main() {
 	next=rand_active();	
 	while (1) {
 		fallen_flag=0;
-		active=next;
-		next=rand_active();
+		if (mode=='l') {
+			active=next_easy();
+		} else {
+			active=next;
+			next=rand_active();
+		}
 		print_next();
 		x=6;
 		y=1;
-		upd_part();
 		if (!check(0, 0)) break;
+		upd_part();
 		while (check(0, 1) || check(-1, 0) || check(1, 0) ||
 				check_clock() || check_aclock()) {
 			key_act();
